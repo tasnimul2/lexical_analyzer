cb326b787cc3832de21aa405bc35b2e48a093dca
Jackie <chowdhury1478@gmail.com>
2021-03-07 00:40:51 -0500

    Assignment 1 Complete

--- Assignment1.java
+++ Assignment1.java
@@ -1,36 +1,28 @@
 import java.io.*;
-import java.util.Scanner;
+
 
 public class Assignment1 {
     public static void main(String[] args){
-        LexicalAnalyzer analyzer = new LexicalAnalyzer(args[0]);
+        try{
+            LexicalAnalyzer analyzer = new LexicalAnalyzer(args[0]);
+            FileWriter writer = new FileWriter("output1.txt");
+            while(true){
+                Token tkn = analyzer.getToken();
+                if(tkn.getSym() == Symbol.EOI){
+                    writer.write( tkn.getSym() + ", " + tkn.getId() + "\n");
+                    break;
+                }else if(tkn.getSym() == Symbol.NUMLIT){
+                    writer.write( tkn.getSym() + ", " + tkn.getNum() + "\n");
+                } else{
+                    writer.write( tkn.getSym() + ", " + tkn.getId() +"\n");
+                }
 
-        /*
-        try {
-            File inFile = new File(args[0]);
-            FileWriter writer = new FileWriter(args[1]);
-            Scanner scan = new Scanner(inFile);
-
-            while(scan.hasNextLine()){
-                String textLine = scan.nextLine();
-                System.out.println(textLine);
-            }
-
-
-            for(int i =0; i <10; i++){
-                writer.write(i + "\n");
             }
             writer.close();
-
-
-        }catch (FileNotFoundException e){
-            System.out.println("Input file does not exist, please make sure the file is located in this folder");
         }catch (IOException e){
-            System.out.println("Issue with I/O operation has occurred");
+            System.out.println("IOException has occurred");
+        }catch (Exception e){
+            System.out.println("Exception in the main method");
         }
-        */
-
-
-
     }
 }
--- LexicalAnalyzer.java
+++ LexicalAnalyzer.java
@@ -1,31 +1,158 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Scanner;
-import java.util.Stack;
+
 
 public class LexicalAnalyzer {
-    private Stack<Token> tokenStack = new Stack<>();
+    private LinkedList<Token> tokenQueue = new LinkedList<>();
+    private HashMap<String,Symbol> symbolList = new HashMap<>();
 
     public LexicalAnalyzer(String fileName){
         File inFile = new File(fileName);
-        findToken(inFile);
+        loadSymbolList();
+        findTokens(inFile);
     }
     public Token getToken(){
-
-        return new Token(Symbol.NAL,"identifier",1234);
+        if(hasToken()){
+            return tokenQueue.poll();
+        }else{
+            tokenQueue.add(new Token(Symbol.EOI,"End of Input",0));
+        }
+        return tokenQueue.poll();
     }
 
-    private void findToken(File inFile){
+    private void loadSymbolList(){
+        symbolList.put("(",Symbol.LPAREN);
+        symbolList.put(")",Symbol.RPAREN);
+        symbolList.put("++",Symbol.POSTINC);
+        symbolList.put("--",Symbol.POSTDEC);
+        symbolList.put("*",Symbol.MULT);
+        symbolList.put("/",Symbol.DIV);
+        symbolList.put("%", Symbol.MOD);
+        symbolList.put("+",Symbol.PLUS);
+        symbolList.put("-",Symbol.MINUS);
+        symbolList.put("<<",Symbol.SLL);
+        symbolList.put(">>",Symbol.SRA);
+        symbolList.put(">>>",Symbol.SRL);
+        symbolList.put("&",Symbol.BITAND);
+        symbolList.put("^",Symbol.BITXOR);
+        symbolList.put("!",Symbol.BITOR);
+        symbolList.put(":=",Symbol.BECOMES);
+        symbolList.put(";",Symbol.SEMICOLON);
+
+
+    }
+
+    private void findTokens(File inFile){
         try {
             Scanner scan = new Scanner(inFile);
+            String identifier ="";
             while(scan.hasNextLine()){
-                String textLine = scan.nextLine();
-                System.out.println(textLine);
+                String textLine = scan.nextLine() + " ";
+
+                for(int i = 0; i < textLine.length();i++){
+                    String lexeme = String.valueOf(textLine.charAt(i));
+                    if(i != textLine.length()-1 && isNumber(lexeme) ){
+                        while(i!= textLine.length()-1 &&  isNumber(String.valueOf(textLine.charAt(i)))){
+                            identifier+= textLine.charAt(i);
+                            ++i;
+                        }
+                        if(needsTrimming(identifier)){
+                            identifier= trim(identifier,10);
+                        }
+                        tokenQueue.add(new Token(Symbol.NUMLIT,identifier,Long.parseLong(identifier)));
+                        identifier = "";
+                    }
+                    lexeme = String.valueOf(textLine.charAt(i));
+                    if(i != textLine.length()-1 && isAlphabet(lexeme) && (isAlphabet(String.valueOf(textLine.charAt(i+1))) || isNumber(String.valueOf(textLine.charAt(i+1))) ) ){
+                        while(i!= textLine.length()-1 && isAlphabet(String.valueOf(textLine.charAt(i))) || isNumber(String.valueOf(textLine.charAt(i)))){
+                            identifier+= textLine.charAt(i);
+                            ++i;
+                        }
+                        if(needsTrimming(identifier)){
+                            identifier = trim(identifier,20);
+                        }
+                        tokenQueue.add(new Token(Symbol.IDENT,identifier,0));
+                        identifier = "";
+                    }
+                    lexeme = String.valueOf(textLine.charAt(i));
+                    if(i != textLine.length()-1 && isAlphabet(lexeme) && !(isAlphabet(""+textLine.charAt(i+1)) || isNumber(""+textLine.charAt(i+1)) )){
+                        tokenQueue.add(new Token(Symbol.IDENT,lexeme,0));
+                    }else if(symbolList.containsKey(lexeme)&& i != textLine.length()-1 && lexeme.equals("+")  && textLine.charAt(i+1) == '+'){
+                        tokenQueue.add(new Token(symbolList.get("++"),"++",0));
+                        ++i;
+                    }else if(symbolList.containsKey(lexeme)&& i != textLine.length()-1 && lexeme.equals("-")  && textLine.charAt(i+1) == '-'){
+                        tokenQueue.add(new Token(symbolList.get("--"),"--",0));
+                        ++i;
+                    }else if(i != textLine.length()-1 && lexeme.equals("<")  && textLine.charAt(i+1) == '<'){
+                        tokenQueue.add(new Token(symbolList.get("<<"),"<<",0));
+                        ++i;
+                    }else if(i != textLine.length()-2 && lexeme.equals(">")  && textLine.charAt(i+1) == '>' && textLine.charAt(i+2) == '>'){
+                        tokenQueue.add(new Token(symbolList.get(">>>"),">>>",0));
+                        i+=2;
+                    }else if(i != textLine.length()-1 && lexeme.equals(">")  && textLine.charAt(i+1) == '>' ) {
+                        tokenQueue.add(new Token(symbolList.get(">>"),">>",0));
+                        ++i;
+                    }else if(i != textLine.length()-1 && lexeme.equals(":")  && textLine.charAt(i+1) == '=' ){
+                        tokenQueue.add(new Token(symbolList.get(":="),":=",0));
+                        ++i;
+                    }
+                    else if(symbolList.containsKey(lexeme)){
+                        tokenQueue.add(new Token(symbolList.get(lexeme),lexeme,0));
+                    }else if(!lexeme.equals(" ")){
+                        tokenQueue.add(new Token(Symbol.NAL,lexeme,0));
+                    }
+                }
             }
         }catch (FileNotFoundException e) {
             System.out.println("Input file does not exist, please make sure the file is located in this folder");
+        }catch (Exception e){
+            System.out.println("an Exception occurred in the findToken method");
         }
     }
+
+    private boolean hasToken(){
+        if(tokenQueue.isEmpty()){
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isAlphabet(String input){
+        char temp = input.charAt(0);
+        if((temp >= 'a' && temp <= 'z') || (temp >= 'A' && temp <= 'Z')){
+            return true;
+        }
+        return false;
+    }
+    private boolean isNumber(String input){
+        try{
+            long isNum = Long.parseLong(input);
+        }catch(NumberFormatException e){
+            return false;
+        }catch (Exception e){
+            return  false;
+        }
+        return true;
+    }
+
+    private boolean needsTrimming(String input){
+        if(isNumber(input) && input.length() > 10){
+            return true;
+        }else if(!isNumber(input) && input.length() > 20){
+            return true;
+        }
+        return false;
+    }
+
+    private String trim(String input,int trimSize){
+        String output = "";
+        for(int i=0;i<trimSize;i++){
+            output+= input.charAt(i);
+        }
+        return output;
+    }
+
 }
